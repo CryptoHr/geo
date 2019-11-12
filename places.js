2258a5fa28dce6542d9f6781cb5629d353d68258
CryptoHr <45811932+CryptoHr@users.noreply.github.com>
2019-11-12 15:24:42 +0100

    Update places.js

--- places.js
+++ places.js
@@ -109,7 +109,7 @@ function renderPlaces(places) {
         const icon = document.createElement('a-image');
         icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
         icon.setAttribute('name', place.name);
-        icon.setAttribute('src', '../assets/energy_marker.png');
+        icon.setAttribute('src', './location.png');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
         icon.setAttribute('scale', '20, 20');
